a29e19842211c228bb9438250b4b49c924ffd092
Mike Ralphson <mike.ralphson@gmail.com>
2016-10-06 18:03:44 +0100

    Update README.md and yaml support

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # shinola
-Shins-as-a-service. Generate Shins/Slate API documentation on the fly
+Shins-as-a-service. Generate beautiful [Shins](https://github.com/mermade/shins) Slate-compatible API documentation on the fly, from markdown or OpenApi / Swagger definitions
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Then connect and submit a request like:
+
+* `http://shinola.herokuapp.com/json?url=http://url.to.swagger.json`
+* `http://shinola.herokuapp.com/yaml?url=http://url.to.swagger.yaml`
+* `http://shinola.herokuapp.com/markdown?url=http://url.to.markdown.md`
+
+The server runs locally on port 5678 or as specified in the `PORT` environment variable
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 var express = require('express');
 var compression = require('compression');
+var yaml = require('js-yaml');
 
 var shins = require('shins/index.js');
 var shinsDir = shins.srcDir();
@@ -12,44 +13,64 @@ var fetch = require('./fetch.js');
 var app = express();
 app.use(compression());
 
-app.get('/', function(req,res) { res.sendFile(path.join(__dirname,'index.html')) });
-app.get('/markdown', function(req,res) {
-  fetch.get(req.query.url,{},{},function(err,resp,obj){
-    if (obj) {
-	  //console.log(obj);
-      res.send(shins.render(obj));
-    }
-    else {
-      res.status(500);
-      res.end();
-    }
-  });
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'index.html'))
 });
-app.get('/json', function(req,res) {
-  fetch.get(req.query.url,{},{},function(err,resp,obj){
-    if (obj) {
-	  //console.log(obj);
-	  obj = widdershins.convert(JSON.parse(obj),req.query.url);
-      shins.render(obj,function(err,str){
-	    //console.log(str);
-		//console.log(err);
-	    res.send(str);
-	  });
-    }
-    else {
-      res.status(500);
-      res.end();
-    }
-  });
+app.get('/markdown', function (req, res) {
+	fetch.get(req.query.url, {}, {}, function (err, resp, obj) {
+		if (obj) {
+			res.send(shins.render(obj));
+		} else {
+			res.status(500);
+			res.end();
+		}
+	});
 });
-app.use("/",  express.static(shinsDir));
+app.get('/json', function (req, res) {
+	fetch.get(req.query.url, {}, {}, function (err, resp, obj) {
+		if (obj) {
+			try {
+				obj = widdershins.convert(JSON.parse(obj), req.query.url);
+				shins.render(obj, function (err, str) {
+					res.send(str);
+				});
+			} catch (ex) {
+				res.status(500);
+				res.end();
+			}
+		} else {
+			res.status(500);
+			res.end();
+		}
+	});
+});
+app.get('/json', function (req, res) {
+	fetch.get(req.query.url, {}, {}, function (err, resp, obj) {
+		if (obj) {
+			try {
+				obj = widdershins.convert(yaml.safeLoad(obj), req.query.url);
+				shins.render(obj, function (err, str) {
+					res.send(str);
+				});
+			} catch (ex) {
+				res.status(500);
+				res.end();
+			}
+		} else {
+			res.status(500);
+			res.end();
+		}
+	});
+});
+app.use("/", express.static(shinsDir));
 
 var myport = process.env.PORT || 5678;
-if (process.argv.length>2) myport = process.argv[2];
+if (process.argv.length > 2)
+	myport = process.argv[2];
 
 var server = app.listen(myport, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Arapaho server listening at http://%s:%s', host, port);
+    console.log('Shinola server listening at http://%s:%s', host, port);
 });
